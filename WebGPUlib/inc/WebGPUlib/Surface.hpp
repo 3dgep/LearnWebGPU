969e3ab73128a3afd8fd18e7ac8b2cb179bad993
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2024-07-28 20:54:10 +0200

    Add (empty) Texture class.

--- WebGPUlib/inc/WebGPUlib/Surface.hpp
+++ WebGPUlib/inc/WebGPUlib/Surface.hpp
@@ -2,6 +2,8 @@
 
 #include <webgpu/webgpu.h>
 
+struct SDL_Window;
+
 namespace WebGPUlib
 {
 class Surface
@@ -9,12 +11,15 @@ class Surface
 public:
     void resize( uint32_t width, uint32_t height );
 
+    WGPUTextureView getNextTextureView();
+
 protected:
-    Surface( WGPUSurface&& surface, const WGPUSurfaceConfiguration& config );
+    Surface( WGPUSurface&& surface, const WGPUSurfaceConfiguration& config, SDL_Window* window );
     virtual ~Surface();
 
 private:
     WGPUSurface              surface;
     WGPUSurfaceConfiguration config;
+    SDL_Window*              window;
 };
 }  // namespace WebGPUlib
--- WebGPUlib/src/Surface.cpp
+++ WebGPUlib/src/Surface.cpp
@@ -1,6 +1,8 @@
 #include <WebGPUlib/Surface.hpp>
 
-#include <utility>
+#include <SDL2/SDL_video.h>
+
+#include <iostream>
 
 using namespace WebGPUlib;
 
@@ -12,10 +14,58 @@ void Surface::resize( uint32_t width, uint32_t height )
     wgpuSurfaceConfigure( surface, &config );
 }
 
+WGPUTextureView Surface::getNextTextureView()
+{
+    WGPUSurfaceTexture surfaceTexture;
+    wgpuSurfaceGetCurrentTexture( surface, &surfaceTexture );
+
+    switch ( surfaceTexture.status )  // NOLINT(clang-diagnostic-switch-enum)
+    {
+    case WGPUSurfaceGetCurrentTextureStatus_Success:
+        // All good. Just continue.
+        break;
+    case WGPUSurfaceGetCurrentTextureStatus_Timeout:
+    case WGPUSurfaceGetCurrentTextureStatus_Outdated:
+    case WGPUSurfaceGetCurrentTextureStatus_Lost:
+    {
+        if ( surfaceTexture.texture )
+            wgpuTextureRelease( surfaceTexture.texture );
+
+        int width, height;
+        SDL_GetWindowSize( window, &width, &height );
+
+        if ( width > 0 && height > 0 )
+        {
+            resize( width, height );
+        }
+
+        return nullptr;
+    }
+    default:
+        // Handle the error.
+        std::cerr << "Error getting surface texture: " << surfaceTexture.status << std::endl;
+        return nullptr;
+    }
+
+    WGPUTextureViewDescriptor viewDescriptor {};
+    viewDescriptor.label           = "Surface Texture View";
+    viewDescriptor.format          = wgpuTextureGetFormat( surfaceTexture.texture );
+    viewDescriptor.dimension       = WGPUTextureViewDimension_2D;
+    viewDescriptor.baseMipLevel    = 0;
+    viewDescriptor.mipLevelCount   = 1;
+    viewDescriptor.baseArrayLayer  = 0;
+    viewDescriptor.arrayLayerCount = 1;
+    viewDescriptor.aspect          = WGPUTextureAspect_All;
+    WGPUTextureView targetView     = wgpuTextureCreateView( surfaceTexture.texture, &viewDescriptor );
+
+    return targetView;
+}
+
 Surface::Surface( WGPUSurface&& _surface,  // NOLINT(cppcoreguidelines-rvalue-reference-param-not-moved)
-                  const WGPUSurfaceConfiguration& _config )
+                  const WGPUSurfaceConfiguration& _config, SDL_Window* _window )
 : surface( _surface )
 , config( _config )
+, window( _window )
 {}
 
 Surface::~Surface()
